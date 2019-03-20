27fadf08a928c2dc2f690fcefd59c16eb754fee9
Web PDI <webpdi.team@gmail.com>
2019-03-20 00:15:33 -0300

    Basic implementation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { ViewsModule } from './views/views.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateModule } from './template/template.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
@@ -9,6 +10,7 @@ import { TemplateModule } from './template/template.module';
     CommonModule,
     ViewsModule,
     TemplateModule,
+    HttpClientModule
   ],
   exports: [
     ViewsModule,
--- src/app/core/views/home/home.component.ts
+++ src/app/core/views/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  messages: string;
+  @Input() url: string = '';
 
   ngOnInit() {
+  }
+
+  shorten(inputUrl: string) {
+    const api = 'http://shortener-env.k6wamyyvfj.us-east-1.elasticbeanstalk.com/v1/shu/shorten';
+    this.http.post(api , { 'url': inputUrl}).subscribe((res) => {
+      this.messages = res['shortenedUrl'];
+      this.url = '';
+    },
+    (resFail) => {
+      this.messages = resFail.error + ' -> Don\'t forget to put the protocol in the beggining: http://';
+      this.url = '';
+    });
   }
 
 }
